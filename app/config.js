849b205b5b8b6d9ff56fafa3262068ee6be57e4f
Hack Reactor Students <accounts@hackreactor.com>
2016-10-13 16:13:06 -0700

    Refactor link model to mongoose

--- app/config.js
+++ app/config.js
@@ -1,5 +1,8 @@
 var path = require('path');
-var MongoClient = require('mongodb').MongoClient;
+// var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
 // var knex = require('knex')({
 //   client: 'sqlite3',
 //   connection: {
@@ -10,43 +13,73 @@ var MongoClient = require('mongodb').MongoClient;
 // var db = require('bookshelf')(knex);
 
 // Connection URL
-var url = 'mongodb://107.170.200.33/data/db';
+var url = 'mongodb://localhost:4568/database';
+mongoose.connect(url);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('db is open!');
+});
+
+
+var linkSchema = exports.linkSchema = new Schema({
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number,
+  createdAt: { type: Date, default: Date.now }
+});
+
+var userSchema = exports.userSchema = new Schema({
+  username: String,
+  passwords: String,
+  createdAt: { type: Date, default: Date.now }
+});
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+// MongoClient.connect(url, function(err, db) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('db: ', db);
+//     console.log("Connected successfully to server");
+//   }
+//   // db.close();
+// });
 
-  db.close();
-});
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+// db.knex.schema.hasTable('urls').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('urls', function (link) {
+//       link.increments('id').primary();
+//       link.string('url', 255);
+//       link.string('baseUrl', 255);
+//       link.string('code', 100);
+//       link.string('title', 255);
+//       link.integer('visits');
+//       link.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
 
-module.exports = db;
+// db.knex.schema.hasTable('users').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('users', function (user) {
+//       user.increments('id').primary();
+//       user.string('username', 100).unique();
+//       user.string('password', 100);
+//       user.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+exports.mongoose = mongoose;
